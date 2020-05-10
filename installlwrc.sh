851d71bde110bba411d75f8eef11166595cba0c4
Lewis Walsh <lewis@lewiswalsh.com>
2020-05-10 17:49:25 +0100

    initial commit

---
+++ installlwrc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LWRC_COUNT=$(grep ".lwrc" ~/.bashrc | wc -l)
+
+if (( $LWRC_COUNT > 0 )); then
+  echo "Source line already exists"
+else
+  echo "Adding 'source ~/.lwrc' to .bashrc"
+  echo "source ~/.lwrc" >> ~/.bashrc
+fi
